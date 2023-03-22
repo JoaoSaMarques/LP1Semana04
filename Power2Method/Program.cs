9901ee9a087b533f5e3a600096be5c571789640c
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-22 18:12:11 +0000

    Exe1

---
+++ Power2Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        private static void Power2Method()
+        {
+            for(int i = 1; i <= (1 << 5); i = i << 1);
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
