435644b37cb279ab048a5483b712c6aec50a1dac
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-03-23 15:42:51 +0000

    Power2MethodDentrodeMain

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,18 +4,32 @@ namespace Power2Method
 {
     class Program
     {
-        private static void Power2Method()
+        //Exe1 + Exe 4.2
+        private static void Power2Method5()
         {
-            for(int i = 1; i <= (1 << 5); i = i << 1);
+            for(int i = 1; i <= (1 << 5); i = i << 1)
             {
-                Console.WriteLine(i);
+                Powers2UntilN(5);
             }
             
         }
-
+        //Exe 4.1
+        private static void Powers2UntilN(int n)
+        {
+            for(int i = 0; i <= n; i = i++)
+            {
+                int powerof2 = (int)Math.Pow(2, i);
+                Console.WriteLine(powerof2);
+            }
+            
+        }
+        //Main
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Power2Method5();
+            Power2Method5();
+            Powers2UntilN(5);
+            Powers2UntilN(10);
         }
     }
 }
