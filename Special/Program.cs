c42dcb2ff6cf9a0528720f1077b44a884fda3fb5
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-03-13 11:39:43 +0000

    added to the method

--- Special/Program.cs
+++ Special/Program.cs
@@ -9,10 +9,16 @@ namespace Special
             Console.WriteLine("Hello, World!");
         }
 
-        private static int GetSpecial(int n);
+        private static int GetSpecial(int n)
         {
-            return true;
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return GetSpecial(n - 1) + 1;
+            }
         }
-
     }
 }
