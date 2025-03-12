9033cc6083252ce69533b48a144cb0ef3d80df0f
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-12 13:31:47 +0000

    Method GetSpecial

--- Special/Program.cs
+++ Special/Program.cs
@@ -4,6 +4,13 @@ namespace Special
 {
     public class Program
     {
+        private static int GetSpecial(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return GetSpecial(n - 1) + GetSpecial(n - 2) + GetSpecial(n - 3);
+        }
+        
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
