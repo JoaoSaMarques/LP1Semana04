e565545e4967dd1d81bbd81ccca61c4e2cfa5ecb
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-03-12 13:32:52 +0000

    XML for GetSpecial

--- Special/Program.cs
+++ Special/Program.cs
@@ -4,13 +4,18 @@ namespace Special
 {
     public class Program
     {
+        /// <summary>
+        /// Recursively calculates the n-th number in the special sequence.
+        /// </summary>
+        /// <param name="n">The index of the number in the special sequence.</param>
+        /// <returns>The n-th number in the special sequence.</returns>
         private static int GetSpecial(int n)
         {
             if (n == 0) return 0;
             if (n == 1) return 1;
             return GetSpecial(n - 1) + GetSpecial(n - 2) + GetSpecial(n - 3);
         }
-        
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
